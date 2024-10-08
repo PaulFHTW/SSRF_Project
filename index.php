0e151cb8ec49adb514c7f677fcf1db7b6f7d8404
PaulFHTW <paul.bogza@gmail.com>
2024-10-08 11:37:30 +0200

    basic structure

--- index.php
+++ index.php
@@ -1,12 +1,44 @@
 <html>
     <head>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="css/index.css">
         <title>SSRF Blog</title>
     </head>
 
     <body>
         <?php include 'includes/navbar.php';?>
+
+        <div class="header">
+        <h2>SSRF Blog</h2>
+        </div>
+
+        <div class="row">
+        <div class="leftcolumn">
+            <div class="card">
+                <h2>My Blog is now extremely unhackable</h2>
+                <h5>Learned from the best, October 3rd 2024</h5>
+                <img src="images/broken_access.png">
+            </div>
+        </div>
+        <div class="rightcolumn">
+            <div class="card">
+                <h2>About Me</h2>
+                <img src="images/hackerman.jpeg" style="height:200px;">
+                <p>Found zero-day in the time protocol implementation of earth v2.024, allowing kung fury to travel to the past.</p><br>
+                <p>Enjoys long walks on the beach</p>
+                
+            </div>
+            <div class="card">
+                <h3>Popular Post</h3>
+                    <a href="https://github.com/XaFF-XaFF/Black-Angel-Rootkit">RootKit</a>
+            </div>
+            <div class="card">
+                <h3>Follow Me</h3>
+                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Twitter</p>
+            </div>
+        </div>
+        </div>
+
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
